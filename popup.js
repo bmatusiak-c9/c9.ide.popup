c91fafc900bb7b234e116682430e4083044f12d1
Bradley Matusiak <usabrad86@bellsouth.net>
2016-01-23 22:00:37 +0000

    major update, cleaned up code, moved code

--- popup.js
+++ popup.js
@@ -5,65 +5,44 @@
  * 
  **/
 define(function(require, exports, module) {
-    main.consumes = ["Plugin", "c9", "commands", "tabManager", "tabbehavior", "menus", "ui", "layout", 
-                "panels", "tree", "settings", "dialog.question", "ace", "breakpoints","popup.windows"];
+    main.consumes = ["Plugin", "c9", "commands", "tabManager", "tabbehavior", "menus", "ui", "layout",
+        "panels", "settings", "popup.windows"
+    ];
     main.provides = ["popup"];
 
     return main;
 
     function main(options, imports, register) {
         var Plugin = imports.Plugin;
-        var tabbehavior = imports.tabbehavior;
+        var c9 = imports.c9;
         var commands = imports.commands;
         var tabManager = imports.tabManager;
+        var tabbehavior = imports.tabbehavior;
         var menus = imports.menus;
         var ui = imports.ui;
-        var ace = imports.ace;
-        var question = imports["dialog.question"].show;
-        var mnuContext = tabbehavior.contextMenu;
-        var settings = imports.settings;
         var layout = imports.layout;
         var panels = imports.panels;
-        var c9 = imports.c9;
-        var $console = imports.console;
-        
+        var settings = imports.settings;
         var windows = imports["popup.windows"];
-        
+
         /***** Initialization *****/
 
         var plugin = new Plugin("Ajax.org", main.consumes);
+
         var emit = plugin.getEmitter();
-        
+
+        var mnuContext = tabbehavior.contextMenu;
+
         plugin.on("load", function() {});
         plugin.on("unload", function() {});
 
-        plugin.windows = {};
-        
-        
-        settings.on("onevent",function(e){
-            //settings.$getEmitter().apply(null,e);
-            windows.emit("settings-onevent",e);
+        settings.on("onevent", function(e) {
+            windows.emit("settings-onevent", e);
         });
-        windows.on("settings-onevent",function(e){
-            settings.$getEmitter().apply(null,e);
+        windows.on("settings-onevent", function(e) {
+            settings.$getEmitter().apply(null, e);
         });
-        
-        
-        var popupWindowNameKey = "c9popup-";
-        var popupWindows;
-        var popupMainWindow = window.opener || window;
-        
-        function isPopup() {
-            return (window.name.substr(0, popupWindowNameKey.length) == popupWindowNameKey);
-        }
-        
-        if(isPopup()){//send all console traffic to main window
-            window.console.warn = popupMainWindow.console.warn.bind(popupMainWindow.console,window.name+":");
-            window.console.log = popupMainWindow.console.log.bind(popupMainWindow.console,window.name+":");
-            window.console.info = popupMainWindow.console.info.bind(popupMainWindow.console,window.name+":");
-            window.console.error = popupMainWindow.console.error.bind(popupMainWindow.console,window.name+":");
-        }
-        
+
         (function(item) {
             commands.addCommand({
                 name: item[0],
@@ -91,214 +70,98 @@ define(function(require, exports, module) {
         });
         menus.addItemByPath("View/Popout Window", itmOpenPopoutWindow, 100, plugin);
 
-        menus.addItemByPath((isPopup() ? "Popin Tab" : "Popout tab"), new ui.item({
+        menus.addItemByPath((windows.isPopup ? "Popin Tab" : "Popout tab"), new ui.item({
             command: "popuptab"
         }), 1020, mnuContext, plugin);
 
-        settings.on("read", function() {
-            settings.setDefaults("state/popup", [
-                ["active", "true"],
-                ["windows", "[]"]
-            ]);
-            
-            var popupWindows = settings.getJson("state/popup/@windows");
-            
-            popupWindows.forEach(function(v,i){
-                openWindow(null, 1000,v.name);
-            });
-            
-        }, plugin);
-    
-        settings.on("write", function (e) {
-            var popupWindows = [];
-            for(var i in plugin.windows){
-                popupWindows.push({name:plugin.windows[i].name});
-            }
-            settings.setJson("state/popup/@windows", popupWindows);
-            
-        });
 
         function popuptab(tab) {
             function tabOpened(err, $tab) {
                 if (err) return console.error(err);
-                tab.meta.$ignore = true;
-                tab.document.meta.$ignoreSave = true;
-                tab.meta.$detach = true;
+                tab.meta.$ignore = true;//terminal
+                tab.meta.$detach = true;//terminal
+                tab.document.meta.$ignoreSave = true;//save
                 tabbehavior.closetab(tab);
             }
-            if (isPopup()) {//send back to main window
+            if (windows.isPopup) { //send back to main window
                 var tabState = tab.getState();
-                tabState.windowName = window.name;
                 tabOpened();
-                setTimeout(function(){
-                    popupMainWindow.app.tabManager.open(tabState);
-                },100);
+                setTimeout(function() {
+                    windows.parent.app.tabManager.open(tabState);
+                }, 100);
             }
             else
-                openWindow(function(popupwin) {
-                    if (tab) {
-                        var tabState = tab.getState();
-                        tabState.windowName = window.name;
-                        tabOpened();
-                        popupwin.app.tabManager.open(tabState);
-                    }
+                windows.openWindow({
+                    ready: function(popupwin) {
+                        if (tab) {//sometime tab is not here when reopening cloud9, so it will load from previous settings tab state
+                            var tabState = tab.getState();
+                            tabOpened();
+                            popupwin.app.tabManager.open(tabState);
+                        }
+                    },
+                    beforeunload: popupBeforeUnload
                 });
         }
-        /*
-        tabManager.on("open", function(e) {
-            for(var i in plugin.windows){
-                var $window = plugin.windows[i];
-                if(!$window || !$window.app) return;
-                var tabsList = plugin.windows[i].app.tabManager.getTabs();
-                for (var j = 0; j < tabsList.length; j++) { 
-                    var $tab = tabsList[j];
-                    if(( $tab.document.editor && $tab.document.editor.type != "terminal") && 
-                        $tab.path == e.tab.path && 
-                        e.options.windowName != i &&
-                        !plugin.windows[e.options.windowName].closed){
-                        setTimeout(function(){
-                            popuptab(e.tab);
-                        },0);
-                        return;
+
+        function popupBeforeUnload($window) {
+            if (!window.closing) {
+                var inter = setInterval(function() {//want to make sure it's not saving settings
+                    if($window.closed){//safe to say cloud9 is no longer alive in popup
+                        settings.set("state/" + $window.name, {}, true); //clearState of popwindow
+                        delete settings.model.state[$window.name];// just to be sure
+                        settings.save(true);
+                        clearInterval(inter);
                     }
-                }
+                }, 10);
             }
-        });*/
-        
-        if (isPopup()) {
-            //popupMainWindow.app.settings.on("onevent",function(e){
-                //settingsEmitter.apply(null,e);
-            //})
-            c9.once("ready",function(){
+        }
+
+        if (windows.isPopup) {
+            c9.once("ready", function() {
                 layout.setBaseLayout("minimal");
                 panels.deactivate("tree");
-                popupMainWindow.app.settings.set("state/popup/@isOpen","true"); //dont re open this popup window on reload
-                popupMainWindow.app.settings.save(true);
             });
-            tabManager.on("ready",function(){
+            tabManager.on("ready", function() {
                 tabManager.toggleButtons(0);
                 window.c9popupReady();
                 emit("ready");
             });
-            menus.on("restore",function(){
+            menus.on("restore", function() {
                 popuptab(tabManager.getPanes()[0].getTabs()[0]);
                 menus.minimize();
             });
             tabManager.on("tabAfterClose", function(e) {
                 if (tabManager.getPanes()[0].getTabs().length == 1 && tabManager.getPanes()[0].getTabs()[0].path == e.tab.path) {
-                    popupMainWindow.app.settings.set("state/" + window.name, {}, true); //clearState of popwindow
-                    popupMainWindow.app.settings.set("state/popup/@isOpen","false");
-                    popupMainWindow.app.settings.save(true);
-                    
-                    setTimeout(function(){
-                        window.location.href = "about:blank";
+                    setTimeout(function() {
                         window.close();
-                    },0);
+                    }, 10);
                 }
-            });
-                
-            window.addEventListener("beforeunload",function() {//popup window
-                window.closing = true;
-                if (!popupMainWindow.closing && tabManager.getPanes()[0].getTabs().length == 1) {
-                    popupMainWindow.app.settings.set("state/popup/@isOpen","false"); //dont re open this popup window on reload
-                    popupMainWindow.app.settings.set("state/"+window.name,{},true); //clearState of popwindow
-                }
-                popupMainWindow.app.settings.save(true);
-            });
-            
-        }else{
-            window.addEventListener("beforeunload",function() { //main window
-                window.closing = true;
-                for (var i in plugin.windows)
-                    plugin.windows[i].close();
             });
         }
-        
-        function openWindow(ready, popupBlockerCheckdelay,windowName) {
-            windowName = windowName || makeid();
-            var popupwin;
-            
-            if(plugin.windows[windowName] && !plugin.windows[windowName].closed){
-                popupwin = plugin.windows[windowName];
-                if (ready)
-                    ready(popupwin);
-                popupwin.focus();
-                return;
-            }else
-                popupwin = window.open("", windowName, "height=800,width=600,titlebar=0,toolbar=0,location=0,status=0,menubar=0");
-             
-            /* todo: remember pos of last time window was open, -set these values in settings on popup windows close */
+        else {
+            settings.on("read", function() {
+                settings.setDefaults("state/popup", [
+                    ["active", "true"],
+                    ["windows", "{}"]
+                ]);
 
-            setTimeout(function() { //this timeout waits for the window to open to check and see if it was blocked by a popup.
-                if (!popupwin || popupwin.outerHeight === 0) {
-                    popupBlocked();
+                var popupWindows = settings.getJson("state/popup/@windows");
+
+                for (var i in popupWindows) {
+                    popupWindows[i].delay = 1000;
+                    popupWindows[i].beforeunload = popupBeforeUnload;
+                    windows.openWindow(popupWindows[i]);
                 }
-                else {
-                    popupwin.c9popupParent = window;
-                    if (popupwin.location.href === "about:blank") {
-                        popupwin.location = window.location; // = window.open(window.location, windowName, "height=800,width=600,titlebar=0,toolbar=0,location=0,status=0,menubar=0");
-                        var intInit;
-                        intInit = setInterval(function() { //this interval waits for the window to load the location it just got set to..
-                            if (popupwin.location.href === window.location.href) {
-                                clearInterval(intInit);
-                                init();
-                            }
-                        });
-                    }
-                    else {
-                        // We've already obtained the reference.
-                        // However, IE and FireFox won't put focus on an already opened window.
-                        // So we have to do that explicitly:
-                        setTimeout(popupwin.focus,0);
-                        if (ready)
-                            ready(popupwin);
-                    }
-                }
-            }, popupBlockerCheckdelay || 100);
 
-            function popupBlocked() {
-                question(
-                    "Popup not visable!",
-                    "Please add this site to your exception list if popup was block. Or the popup window may not have a good width.",
-                    "Open Popup window?",
-                    function(all) { // Yes
-                        openWindow(ready, popupBlockerCheckdelay); //retry everything
-                    },
-                    function(all) { // No
-                        //do nothing as this just closes the dialog
-                    }
-                );
-            }
+            }, plugin);
 
-            function init() {
-                popupwin.c9popupReady = function() {
-                    if (ready)
-                        ready(popupwin);
-                };
-                plugin.windows[popupwin.name] = popupwin;
-                windows.addWindow(popupwin,function(){
-                    delete plugin.windows[popupwin.name];
-                });
-                //postMessage.addWindow(popupwin);
-            }
+            settings.on("write", function(e) {
+                settings.setJson("state/popup/@windows", windows.metadata);
+            });
         }
-
-        function makeid() {
-            var text = "";
-            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-            for (var i = 0; i < 5; i++)
-                text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-            return popupWindowNameKey + text;
-        };
 
         plugin.freezePublicAPI({
-            /* _events: [
-                 
-             ],*/
-            isPopup:isPopup,
-            popuptab:popuptab
+            popuptab: popuptab
         });
 
         register(null, {
--- windows.js
+++ windows.js
@@ -5,13 +5,14 @@
  * 
  **/
 define(function(require, exports, module) {
-    main.consumes = ["Plugin"];
+    main.consumes = ["Plugin", "dialog.question"];
     main.provides = ["popup.windows"];
 
     return main;
 
     function main(options, imports, register) {
         var Plugin = imports.Plugin;
+        var question = imports["dialog.question"].show;
 
         /***** Initialization *****/
 
@@ -19,19 +20,46 @@ define(function(require, exports, module) {
 
         plugin.on("load", function() {});
         plugin.on("unload", function() {});
-        
+
         var emit = plugin.getEmitter();
-        
+
+        var windowNameKey = "c9popup-";
         var windows = [];
 
-        function postMessage(args) {
+        if (isPopup()) { //send all console traffic to main window
+            window.console.warn = window.opener.console.warn.bind(window.opener.console, window.name + ":");
+            window.console.log = window.opener.console.log.bind(window.opener.console, window.name + ":");
+            window.console.info = window.opener.console.info.bind(window.opener.console, window.name + ":");
+            window.console.error = window.opener.console.error.bind(window.opener.console, window.name + ":");
+        }
+        else {
+            window.addEventListener("beforeunload", function() { //main window
+                window.closing = true;
+                for (var j = 0; j < windows.length; j++) {
+                    windows[j].close();
+                }
+            });
+        }
+
+        window.addEventListener("message", function(event) {
+            if (event.origin !== window.location.origin)
+                return;
+            try {
+                var $data = JSON.parse(event.data);
+                if ($data.eventId == "popup") {
+                    emit.apply(null, $data.args);
+                }
+            }
+            catch (e) {}
+        }, false);
+
+
+        function postMessage(args, broadcast) {
             var $data = {
                 eventId: "popup",
                 args: args
             };
-
             var jsonData = JSON.stringify($data);
-
             if (window.opener)
                 window.opener.postMessage(jsonData, window.location.origin);
             else
@@ -40,60 +68,180 @@ define(function(require, exports, module) {
                 }
         }
 
-        function addWindow(window,beforeunload) {
-            windows.push(window);
-            window.addEventListener("beforeunload", function() {
-                delWindow(window);
-                if(beforeunload) beforeunload();
+        function addWindow($window,beforeunload) {
+            windows.push($window);
+            $window.addEventListener("beforeunload", function() {
+                $window.closing = true;
+                if (!window.closing)
+                    for (var j = 0; j < windows.length; j++) {
+                        if (windows[j] === $window || windows[j].name == $window) {
+                            windows.splice(j, 1);
+                        }
+                    }
+                if(typeof beforeunload == "function") beforeunload($window);
             });
         }
 
-        function delWindow(popupName) {
-            for (var j = 0; j < windows.length; j++) {
-                if (windows === popupName || windows[j].name == popupName) {
-                    windows.splice(j, 1);
+        function openWindow(meta) {
+            var windowName = meta.name || makeid();
+            var $window;
+
+            var windowOptions = [
+                ["height", meta.screen ? meta.screen.height : "600"],
+                ["width", meta.screen ? meta.screen.width : "800"],
+                ["titlebar", "0"],
+                ["toolbar", "0"],
+                ["location", "0"],
+                ["status", "0"],
+                ["menubar", "0"]
+            ];
+            if (meta.screen) {
+                if (meta.screen.top) windowOptions.push(["top", meta.screen.top]);
+                if (meta.screen.left) windowOptions.push(["left", meta.screen.left]);
+            }
+            meta.options = windowOptions;
+
+            $window = window.open("", windowName, (function(windowOptions) {
+                var str = [];
+                windowOptions.forEach(function(v, i) {
+                    str.push(v.join("="));
+                });
+                str = str.join(",");
+                console.log("str", str);
+                return str;
+            })(windowOptions));
+            
+            $window.$meta = meta;
+            
+            setTimeout(function() { //this timeout waits for the window to open to check and see if it was blocked by a popup.
+                if (!$window || $window.outerHeight === 0) {
+                    popupBlocked();
                 }
+                else {
+                    if ($window.location.href === "about:blank") {
+                        $window.location = window.location; // = window.open(window.location, windowName, "height=800,width=600,titlebar=0,toolbar=0,location=0,status=0,menubar=0");
+                        var intInit;
+                        intInit = setInterval(function() { //this interval waits for the window to load the location it just got set to..
+                            if ($window.location.href === window.location.href) {
+                                clearInterval(intInit);
+                                init();
+                            }
+                        });
+                    }
+                    else {
+                        // We've already obtained the reference.
+                        // However, IE and FireFox won't put focus on an already opened window.
+                        // So we have to do that explicitly:
+                        $window.focus();
+                        if (meta.ready)
+                            meta.ready($window);
+                    }
+                }
+            }, meta.delay || 100);
+
+            function popupBlocked() {
+                question(
+                    "Popup not visable!",
+                    "Please add this site to your exception list if popup was block. Or the popup window may not have a good width.",
+                    "Open Popup window?",
+                    function(all) { // Yes
+                        openWindow(meta); //retry everything
+                    },
+                    function(all) { // No
+                        //do nothing as this just closes the dialog
+                    }
+                );
+            }
+
+            function init() {
+                $window.c9popupReady = function() {
+                    if (meta.ready)
+                        meta.ready($window);
+                };
+                addWindow($window,meta.beforeunload);
             }
         }
-        
-        window.addEventListener("message", function(event) {
 
-            // Do we trust the sender of this message?
-            if (event.origin !== window.location.origin)
-                return;
-            
-            try{
-                var $data = JSON.parse(event.data);
-                if($data.eventId == "popup"){
-                    emit.apply(null,$data.args);   
-                }
-            }catch(e){
-                
+        function makeid() {
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            for (var i = 0; i < 5; i++)
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+            return windowNameKey + text;
+        }
+
+        function getWindowMetadata() {
+            var windowMetadata = {};
+            for (var i in windows) {
+                windowMetadata[windows[i].name] = {
+                    name: windows[i].name,
+                    screen: {
+                        left: windows[i].screenLeft,
+                        top: windows[i].screenTop,
+                        width: windows[i].innerWidth,
+                        height: windows[i].innerHeight
+                    }
+                };
             }
+            return windowMetadata;
+        }
 
+        function isPopup() {
+            return (window.name.substr(0, windowNameKey.length) == windowNameKey);
+        }
 
-        }, false);
-        
+        /**
+         * 
+         **/
         plugin.freezePublicAPI({
-
-            addWindow: addWindow,
-
-            delWindow: delWindow,
-
+            /**
+             * 
+             */
+            openWindow: openWindow,
+            
+            /**
+             * 
+             */
             get windows() {
                 return windows.slice(0);
             },
             
-            get parent(){
+            /**
+             * 
+             */
+            get parent() {
                 return window.opener;
             },
             
-            loopWindows: function(fn){
+            /**
+             * 
+             */
+            get metadata() {
+                return getWindowMetadata();
+            },
+            
+            /**
+             * 
+             */
+            get isPopup() {
+                return isPopup();
+            },
+            
+            /**
+             * 
+             */
+            loopWindows: function(fn) {
                 for (var j = 0; j < windows.length; j++) {
-                    if(windows[j].app && ! windows[j].closing)
-                    if(fn(windows[j])) break;
+                    if (windows[j].app && !windows[j].closing)
+                        if (fn(windows[j])) break;
                 }
             },
+            
+            /**
+             * 
+             */
             emit: function() {
                 var args = [];
                 for (var j = 0; j < arguments.length; j++) {
@@ -102,7 +250,16 @@ define(function(require, exports, module) {
                 postMessage(args);
             },
             
-            //broadcast:broadcast  //send to all windows
+            /**
+             * 
+             */
+            broadcast: function() {//does not work yet
+                var args = [];
+                for (var j = 0; j < arguments.length; j++) {
+                    args.push(arguments[j]);
+                }
+                postMessage(args, true);
+            },
         });
 
         register(null, {
